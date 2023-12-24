787a545e888b37fc7840cd3beee2ad8997bfe27b
Shamonti <67649277+Shamonti@users.noreply.github.com>
2023-12-24 13:17:19 +0600

    basic card

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import './App.css';
 
 function App() {
+  const title = 'Do Market Research';
+  const id = 'BUS-1';
+  const points = 5;
   return (
     <div>
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+      <h1 className="m-2 rounded-lg border bg-gray-100 px-2 ">
+        <div className="py-2 text-base font-semibold">{title}</div>
+        <div className="flex justify-between py-2 text-gray-700">
+          <div>{id}</div>
+          <div>{points}</div>
+        </div>
+      </h1>
     </div>
   );
 }
