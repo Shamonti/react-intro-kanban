f9c240fb961df5038f3f5b6ae4a7077ce7bdd00d
Shamonti <67649277+Shamonti@users.noreply.github.com>
2024-01-03 20:46:47 +0600

    Added task status

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
 import './App.css';
 import TaskCard from './components/TaskCard';
-import { tasks } from './utilities/data-tasks';
+import { statuses, tasks } from './utilities/data-tasks';
 
 function App() {
+  const columns = statuses.map((status) => {
+    const taskInColumn = tasks.filter((task) => task.status === status);
+
+    return {
+      title: status,
+      tasks: taskInColumn,
+    };
+  });
+
   return (
     <>
-      {tasks.map((task) => (
-        <TaskCard task={task} />
-      ))}
+      <div className="flex divide-y">
+        {columns.map((column) => (
+          <div>
+            <h1>{column.title}</h1>
+            {column.tasks.map((task) => (
+              <TaskCard task={task} />
+            ))}
+          </div>
+        ))}
+      </div>
     </>
   );
 }
--- src/utilities/data-tasks.ts
+++ src/utilities/data-tasks.ts
@@ -1,22 +1,30 @@
+export type Status = 'todo' | 'pending' | 'done';
+
 export type Task = {
   title: string;
   id: string;
+  status: Status;
   points?: number;
 };
+
+export const statuses: Status[] = ['todo', 'pending', 'done'];
 
 export const tasks: Task[] = [
   {
     title: 'Do Market Research',
     id: 'BUS-1',
+    status: 'todo',
     points: 5,
   },
   {
     title: 'Computer Analysis',
     id: 'BUS-2',
+    status: 'pending',
   },
   {
     title: 'Develop Marketing Strategy',
     id: 'BUS-4',
     points: 5,
+    status: 'done',
   },
 ];
