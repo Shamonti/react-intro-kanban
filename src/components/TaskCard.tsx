4bf7c8249787492d252d0a32c32514d5187ff5f4
Shamonti <67649277+Shamonti@users.noreply.github.com>
2024-01-04 01:25:15 +0600

    Added priority icons

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,12 +1,67 @@
 import { Task } from '../utilities/data-tasks';
 
+const lowPriorityIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    width="24"
+    height="24"
+    fill="none"
+    stroke="#00CFFF"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <path d="M6 9l6 6 6-6" />
+  </svg>
+);
+
+const mediumPriorityIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    width="24"
+    height="24"
+    fill="none"
+    stroke="#FFA500"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <line x1="4" y1="12" x2="20" y2="12" />
+    <line x1="4" y1="16" x2="20" y2="16" />
+  </svg>
+);
+
+const highPriorityIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    width="24"
+    height="24"
+    fill="none"
+    stroke="#FF2000"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <path d="M6 15l6-6 6 6" />
+  </svg>
+);
 const TaskCard = ({ task }: { task: Task }) => {
   return (
     <div>
       <div className="m-2 w-56 rounded-lg border bg-gray-50 px-2">
         <div className="py-2 text-base font-semibold">{task.title}</div>
         <div className="flex justify-between py-2 text-gray-500">
-          <div>{task.id}</div>
+          <div className="flex gap-2">
+            <div>{task.id}</div>
+            <div>
+              {task.priority === 'high' && lowPriorityIcon}
+              {task.priority === 'medium' && mediumPriorityIcon}
+              {task.priority === 'low' && highPriorityIcon}
+            </div>
+          </div>
           <div>{task.points}</div>
         </div>
       </div>
--- src/utilities/data-tasks.ts
+++ src/utilities/data-tasks.ts
@@ -1,30 +1,36 @@
 export type Status = 'todo' | 'pending' | 'done';
+export type Priority = 'low' | 'medium' | 'high';
 
 export type Task = {
   title: string;
   id: string;
   status: Status;
+  priority: Priority;
   points?: number;
 };
 
 export const statuses: Status[] = ['todo', 'pending', 'done'];
+export const priorities: Priority[] = ['low', 'medium', 'high'];
 
 export const tasks: Task[] = [
   {
     title: 'Do Market Research',
     id: 'BUS-1',
     status: 'todo',
+    priority: 'high',
     points: 5,
   },
   {
     title: 'Computer Analysis',
     id: 'BUS-2',
     status: 'pending',
+    priority: 'medium',
   },
   {
     title: 'Develop Marketing Strategy',
     id: 'BUS-4',
-    points: 5,
     status: 'done',
+    priority: 'low',
+    points: 5,
   },
 ];
